132cfe934f4ac51324976948597e22bde2dc964a
aphrodite000 <153118976@qq.com>
2019-12-16 19:33:09 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public void exist(char[][] board, String word) {
+        char[] a =word.toCharArray();
+        for(int i=0;i<=a.length;i++){
+        }
+    }
+}
